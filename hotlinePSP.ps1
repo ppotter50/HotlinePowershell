732886182c41f0a3fe1b57e6b070bdd663da3f11
ppotter50 <ppotter50@yahoo.com>
2018-06-07 15:36:56 -0400

    Parameterized the hasAdobe function and updated the help text
    Changed the interactive text interface to a parametrized function
    Updated help text to reflect the new syntax

--- hotlinePSP.ps1
+++ hotlinePSP.ps1
@@ -1,45 +1,75 @@
 <#
 
 .SYNOPSIS
-This is a script used to determine if a computer has access to the Adobe CC Suite
+	This is a script used to determine if a computer has access to the Adobe CC Suite
 
 .DESCRIPTION
-This script will either tell you wheter or not a computer has access to the Adobe CC Suite or it will generate a list of computers that do have access to the Adobe CC Suite based on a wildcard search.
+	This script will either tell you whether or not a computer has access to the Adobe CC Suite or it will generate a list of computers that do have access to the Adobe CC Suite based on a search.
 
-This script acheives this by pulling all active directory computers with the identity "CAEN CLSE 2017 ADOBE CC" and sorts them based on a string input
+	This script acheives this by pulling all active directory computers with the identity "CAEN CLSE 2017 ADOBE CC" and sorts them based on a string input
 
-If a hostname is entered the script will look for an exact match and give a true or false response as to whether the computer has access to Adobe CC
-If a hostname is not provided, and instead a wildcard search is entered the script will display a list of all of the hostnames matching that search
+	If a hostname is entered the script will look for an exact match and give a true or false response as to whether the computer has access to Adobe CC
+	If a search is entered the script will display a list of all of the hostnames matching that search
+
+.SYNTAX
+	hasAdobe [-Hostname] <string>
+
+	hasAdobe -Search [<string>]
+
+.PARAMETERS
+	-Hostname <string>
+		Enter the hostname you would like to check for Adobe CC access
+
+	-Search <string>
+		Searches for hostnames that contain the entered string
+		If no string is entered all hosts with Adobe CC access will be listed
+
+.EXAMPLE
+	#Test if hostname CAEN-HOTLINEP05 has Adobe CC access
+	hasAdobe -Hostname "CAEN-HOTLINEP05"
+
+.EXAMPLE
+	#Get all computers with Adobe CC access whose uniquename contains "dc"
+	hasAdobe -Search "DC"
 
 .NOTES
-Currently this function is being updated to be parameterized to make it a single line command without the interactive text interface.
+	Future development: get hostname parameter to take multiple inputs and input from a file
 	
 #>
 
 function hasAdobe {
-	#Ask if user has hostname
-	$hasHost = Read-Host -Prompt "Do you have a hostname? (Y/N)"
-	if ($hasHost -eq "y") {
-		#get hostname
-		$hostnm = Read-Host -Prompt "Enter the hostname"
-	
+
+	#define parameters
+	[CmdletBinding(DefaultParameterName='Hostname')]
+	param (
+		[Parameter(Mandatory,ParameterSetName='Hostname')]
+		[string]$Hostname,
+
+		[Parameter(Mandatory,ParameterSetName='Search')]
+		[string]$Search
+	)
+
+	#Determine which parameter is defined
+	if (-not $Hostname -eq "") {
+
 		#test if there is a computer with this hostname with access to Adobe CC
-		$result = Get-ADGroupMember -Identity "CAEN CLSE 2017 ADOBE CC" | Where-Object{$_.name -eq $hostnm} | Select-Object -property "name"
+		$result = Get-ADGroupMember -Identity "CAEN CLSE 2017 ADOBE CC" | Where-Object{$_.name -eq $Hostname} | Select-Object -property "name"
 		if (-not $result -eq ""){
 			Write-Host "This machine has access to Adobe CC."
 		}
 		else {Write-Host "This machine does not have access to Adobe CC"}
 	}
 
-	elseif ($hasHost -eq "n") {
-		#get search with wildcard
-		$hostApprx = Read-Host -Prompt "Enter a string with a wildcard to guide search (to get all hostname enter only a wildcard)"
-	
+	elseif (-not $Search -eq "") {
+		#Surround search in wildcards
+		$Search=$Search.PadRight($search.length + 1,'*')
+		$Search=$Search.PadLeft($search.length + 1,'*')
+
 		#generate list of computers matching that wildcard search with access to Adobe CC
-		$result = Get-ADGroupMember -Identity "CAEN CLSE 2017 ADOBE CC" | Where-Object{$_.name -like $hostApprx} | Select-Object -property "name"
+		$result = Get-ADGroupMember -Identity "CAEN CLSE 2017 ADOBE CC" | Where-Object{$_.name -like $Search} | Select-Object -property "name" | Sort-Object -Property "name"
 	
 		#output result
-		if ($hostApprx -eq "*") {
+		if ($Search -eq "**") {
 			Write-Host "Here is a list of all hostnames with access to Adobe CC"
 			echo $result
 		}
@@ -47,9 +77,8 @@ function hasAdobe {
 			Write-Host "Here is a list of hostnames matching your search with access to Adobe CC"
 			echo $result
 		}
-		elseif ($result -eq $null) {
+		elseif ($result -eq "") {
 			Write-Host "Your wildcard matched no hostnames with access to Adobe CC."
 		}
-	
-	else {Write-Host "Please enter just ""y"" or""n"""}
+	}
 }
